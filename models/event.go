818adbc1199bfa789933e5d2a074bf617a63e1b3
Josh Butler <josh@emkaytec.io>
2024-10-04 17:21:41 -0500

    Update events

--- models/event.go
+++ models/event.go
@@ -36,6 +36,23 @@ func (e *Event) Save() error {
 	return nil
 }
 
+func (e *Event) Update() error {
+	query := `
+	UPDATE events
+	SET name = ?, description = ?, location = ?, dateTime = ?
+	WHERE id = ?
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.ID)
+	return err
+}
+
 func GetAllEvents() ([]Event, error) {
 	query := "SELECT * FROM events"
 	stmt, err := db.DB.Prepare(query)
