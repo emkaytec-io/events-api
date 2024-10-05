bcfc96e72a3312da54872249c97f3add86fecc61
Josh Butler <josh@emkaytec.io>
2024-10-05 10:33:15 -0500

    Route protection on create event route

--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"emkaytec.io/events-api/v2/models"
+	"emkaytec.io/events-api/v2/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -39,8 +40,24 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Not authorized.",
+		})
+		return
+	}
+
+	err := utils.VerifyToken(token)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Not authorized.",
+		})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
