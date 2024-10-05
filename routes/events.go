8d55447c25cc2abea6a2c50c2935825c53738f2a
Josh Butler <josh@emkaytec.io>
2024-10-05 10:43:21 -0500

    Retrieving and storing user and event ID

--- routes/events.go
+++ routes/events.go
@@ -48,7 +48,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	err := utils.VerifyToken(token)
+	userId, err := utils.VerifyToken(token)
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{
 			"message": "Not authorized.",
@@ -66,7 +66,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.UserID = 1
+	event.UserID = userId
 
 	err = event.Save()
 	if err != nil {
